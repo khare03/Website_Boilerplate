714b4cd95958616b1da2de8f76f69a4070d4f6f1
khare03 <akpk0312@gmail.com>
2017-11-13 17:19:18 -0500

    actions login and register updated

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.post('/register', function(req, res, next) {
+    res.send('success');
+});
+
+router.post('/login', function (req,res,next) {
+  res.send('logged in')  ;
+});
+
+
+
+
+
+
 module.exports = router;
